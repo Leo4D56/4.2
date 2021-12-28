2b95596f2218ce01ca5372ad4e9514ec54b3f564
Lev <lev.popov.777@mail.ru>
2021-12-28 19:30:16 +0700

    Start

--- 4_2/4_2/Program.cs
+++ 4_2/4_2/Program.cs
@@ -4,9 +4,46 @@ namespace _4_2
 {
     class Program
     {
+        private static double A, B;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                PrintMenu();
+                DoAction(Console.ReadLine());
+            }
+        }
+
+        static void PrintMenu() =>
+            Console.WriteLine("1: Input A\n2: Input B\n3: +\n4: -\n5: *\n6: /");
+
+        static void DoAction(string input)
+        {
+            switch (input)
+            {
+                case "1":
+                    InputA(Console.ReadLine());
+                    break;
+                case "2":
+                    InputB(Console.ReadLine());
+                    break;
+                case "3":
+                    Plus(A, B);
+                    break;
+                case "4":
+                    Minus(A, B);
+                    break;
+                case "5":
+                    Multiply(A, B);
+                    break;
+                case "6":
+                    Divide(A, B);
+                    break;
+                default:
+                    Console.WriteLine("Аоаоаоао");
+                    break;
+            }
         }
     }
 }
